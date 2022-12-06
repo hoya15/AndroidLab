6616ab90039aaf826ecf0e44b35361b34b3da21e
hoya15 <hoya15.lee@gmail.com>
2022-12-06 16:49:19 +0900

    11122ㄱㄷㅅㄷㄳ

---
+++ ch5_event/src/main/java/com/example/ch5_event/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ch5_event
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
